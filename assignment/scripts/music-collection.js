2ec87e41cf678feb59de027aae0cceee7bb66a3f
pthomas0309 <preston.thomas355@gmail.com>
2021-05-02 12:52:50 -0500

    created empty array called collection / created function that will take a title, artist, and the year published and turn those values into an object, push that object to the collection array and return the new object / tested the function by adding 6 objects and console logged the collection array

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,1 +1,21 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+  console.log('Adding an album to my collection');
+  let vinyl = {
+    titleOfAlbum: title,
+    recordingArtist: artist,
+    published: yearPublished
+  }
+  collection.push(vinyl)
+  return vinyl
+}
+console.log(addToCollection('Sheer Heart Attack', 'Queen', '1974'));
+console.log(addToCollection('The Power to Believe', 'King Crimson', '2003'));
+console.log(addToCollection('LP1', 'FKA twigs', '2014'));
+console.log(addToCollection('Zaba', 'Glass Animals', '2014'));
+console.log(addToCollection('Disintegration', 'The Cure', '1989'));
+console.log(addToCollection('Watching Movies with the Sound Off', 'Mac Miller', '2013'));
+console.log(collection);
